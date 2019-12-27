be569c8a0bb5617d37724625585a17b8ecbf3f36
Alexandre de Moraes <axmoraes@hotmail.com>
2019-12-27 15:18:01 -0500

    prevent same username with case insensitive on DB

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ var mongoose                = require("mongoose"),
 var UserSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    username: String,
+    email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
     password: String
 });
 
